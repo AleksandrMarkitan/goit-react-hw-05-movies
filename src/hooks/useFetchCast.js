bbd7a2e1c62b2ba54b20cd881b991fe674bdf2ef
AleksandrMarkitan <0688138@gmail.com>
2022-10-23 13:30:09 +0300

    q

--- src/hooks/useFetchCast.js
+++ src/hooks/useFetchCast.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { FetchCast } from '../utils/fetchCast';
+import { FetchCast } from 'utils/fetchCast';
 import { useParams } from 'react-router-dom';
 
 export const useFetchCast = () => {
