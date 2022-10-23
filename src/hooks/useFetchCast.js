932808f168f3ee9b6329a06c7db7451c26e2a4f7
AleksandrMarkitan <0688138@gmail.com>
2022-10-23 15:09:51 +0300

    fix

--- src/hooks/useFetchCast.js
+++ src/hooks/useFetchCast.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { FetchCast } from 'utils/fetchCast';
+import { FetchCast } from 'utils/fetchCasts';
 
 export const useFetchCast = () => {
   const [cast, setCast] = useState(null);
